43874a3ddcb56cd6aa174471607e78a7cf9af744
RoshanArun3 <roshan.arun@live.com>
2023-07-28 02:20:30 -0700

    Dashboard Center Update
    Updated center boxes

---
+++ .history/emto/reactfirst/src/pages/DashBoard/DashBoard_20230728020659.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import IMAGES from '../../Components';
+
+const DashBoard = () => {
+    return (
+        <div class="DashBoard">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <div class="wrapper">
+                <div class="dash">
+                    <div class="top1">
+                        <img src={IMAGES.emtologo} class="dashboardlogo" alt="dashboardlogo" /><br></br>
+                    </div>
+                    <div class="top2">
+                        <label class="intro">Hello, John Peter</label>
+                        <label class="Calender">05 June 2023</label>
+                        <img src={IMAGES.calender} class="calenderIcon" alt="dashboardlogo" /><br></br>
+                        <label class="introsub">Track bookings here. You almost reach a goal!</label>
+                    </div>
+                    <div class="top3">
+                        <img src={IMAGES.profile} class="profileIcon" alt="dashboardlogo" />
+                        <label class="userLabel">John Peter</label><br></br>
+                        <label class="userLabel2">0123456789</label><br></br>
+                        <label class="userLabel2">0.0</label><br></br>
+                    </div>
+
+                    <div class="bottom1">
+                        <img src={IMAGES.dashboard} class="dashboardicon" alt="dashboardlogo" /><label class="dashboardlabels">Dashboard</label><br></br>
+                        <img src={IMAGES.senders} class="dashboardicon" alt="dashboardlogo" /><label class="dashboardlabels">Senders</label><br></br>
+                        <img src={IMAGES.recievers} class="dashboardicon" alt="dashboardlogo" /><label class="dashboardlabels">Recievers</label><br></br><br></br>
+                        <label class="pro">Upgrade to Pro</label>
+                        <label class="unlock">Get 1 Month Free</label>
+                        <label class="unlock2">and Unlock</label><br></br>
+                        <button class="upgrade">Upgrade</button>
+
+                        <img src={IMAGES.settings} class="dashboardicon" alt="dashboardlogo" /><label class="dashboardlabels">Settings</label><br></br>
+                        <img src={IMAGES.csx} class="dashboardicon" alt="dashboardlogo" /><label class="dashboardlabels">Help Desk</label><br></br>
+                        <img src={IMAGES.logout} class="dashboardicon" alt="dashboardlogo" /><label class="dashboardlabels">Logout</label><br></br>
+                    </div>
+
+                    <div class="bottom2">
+
+                        <div class="box">
+                            <img src={IMAGES.booktrips} class="boximage" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Book Trips</label>
+                            <label class="boxlabel3">asdf</label>
+
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.currenttTrips} class="boximage" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Track Current Trip</label>
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.bookedTrips} class="boximage2" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">All Booked Trips</label>
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.incomingShipments} class="boximage2" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Incoming Shipments</label>
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.payments} class="boximage2" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Payments</label>
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.fareCalc} class="boximage2" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Fare Calculator</label>
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.alerts} class="boximage2" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Alerts</label>
+
+                        </div>
+                        <div class="box">
+                            <img src={IMAGES.messages} class="boximage2" alt="dashboardlogo" />
+                            <label class="boxLabel">0</label><br></br>
+                            <label class="boxlabel2">Messages</label>
+
+                        </div>
+                        <div class="box">
+                            <label class="more">more...</label><br></br>
+
+
+                        </div>
+
+                    </div>
+
+                    <div class="bottom3">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default DashBoard;
---
+++ .history/emto/reactfirst/src/pages/Login/Login_20230728021526.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import IMAGES from '../../Components';
+
+const Login = () => {
+    return (
+        <div class="login">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <div class="wrapper">
+
+                <div class="page-left">
+                    <img src={IMAGES.emtologo} class="logo" alt="logo" />
+                    <img src={IMAGES.emtotruck} class="truck" alt="logo" />
+                    <img src={IMAGES.emtocaption} class="emtocaption" alt="logo" />
+                    <div class="box">
+                        <p class="details">EMTO app assigns the shipment to the nearest driver for pick up. You will see your vehicle, driver details, fare and receive real time status updates until delivery.</p>
+                        <a class="link" href="url">Visit Website</a>
+                    </div>
+                </div>
+
+                <div class="page-right">
+                    <div class="signin-content">
+                        <label class="signin-label">Sign In</label> <br></br><br></br>
+                        <label class="num-label">Enter your mobile number</label><br></br>
+                        <input class="numinput" type="number" id="num" name="num" /><span class="small"></span><br></br>
+                        <label class="invalidnumber">Invalid mobile number</label><br></br>
+                        <label class="OTP-label">OTP</label>
+                        <input class="OTPNUM-input" type="number" id="num1" maxlength="4" />
+                        <input class="OTPNUM-input" type="number" id="num2" maxlength="4" />
+                        <input class="OTPNUM-input" type="number" id="num3" maxlength="4" />
+                        <input class="OTPNUM-inputend" type="number" id="num4" maxlength="4" /><br></br>
+                        <label class="invalidOTP">Invalid otp</label>
+                        <button class="submit-button" type="button">SIGN IN</button> <br class="small"></br>
+
+
+                    </div>
+                    <div class="bottom">
+                        <label class="terms">EMTO 2023 . TERMS . PRIVACY</label>
+                        <label class="language" for="html">TAMIL</label>
+                        <input class="radio-button" type="radio" name="lang" value="English" />
+                        <label class="language" for="html">ENGLISH</label>
+                        <input class="radio-button" type="radio" name="lang" value="Tamil" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default Login;
