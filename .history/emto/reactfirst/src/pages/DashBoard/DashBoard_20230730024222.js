fa21bf627d80aafb46760946506309bd8d56bb53
RoshanArun3 <roshan.arun@live.com>
2023-07-30 03:31:28 -0700

    Dashboard Update
    Fixed Right side of dashboard

--- .history/emto/reactfirst/src/pages/DashBoard/DashBoard_20230730024222.js
+++ .history/emto/reactfirst/src/pages/DashBoard/DashBoard_20230730024222.js
@@ -85,10 +85,19 @@ const DashBoard = () => {
                     <div class="bottom-profile">
                         <div class='box2'>
                             <img src={IMAGES.bronze} class="bronze" alt="dashboardlogo" />
+                            {/* <img src={IMAGES.profile} class="bronzeicon" alt="dashboardlogo" />
                             <label class="bronzelabel">Xuvi Bronze</label><br></br>
-                            <iframe class="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.274257380938!2d-70.56068388481569!3d41.45496659976631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e52963ac45bbcb%3A0xf05e8d125e82af10!2sDos%20Mas!5e0!3m2!1sen!2sus!4v1671220374408!5m2!1sen!2sus"></iframe>
+                            <label class="bronzeInformation">0</label>
+                            <label class="bronzeInformation">0</label>
+                            <label class="bronzeInformation">0</label>
+                            <label class="bronzeInformation2">Bookings</label><br></br>
+                            <label class="bronzeInformation2">Pending Payments</label><br></br>
+                            <label class="bronzeInformation2">Points</label><br></br> */}
 
                         </div>
+
+
+                        {/* <img src={IMAGES.bronze} class="box-banner2" alt="dashboardlogo" /> */}
                     </div>
                 </div>
             </div>
